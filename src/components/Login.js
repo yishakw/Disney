68d94a7df4fe408d1ff172ad3165e0589f7e31d0
Yishak Wondimgezahu <yishakwondimgezahu12@gmail.com>
2024-01-20 22:51:50 +0300

    starter files setup and install all the starting packages and dependencies

---
+++ src/components/Login.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+function Login() {
+  return (
+    <Container>
+      <Content>Content</Content>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  overflow: hidden;
+  display: flex;
+`;
+const Content = styled.div``;
+export default Login;
