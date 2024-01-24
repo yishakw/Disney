9558d503673adf67e388084d16a7ac592dffab58
Yishak Wondimgezahu <yishakwondimgezahu12@gmail.com>
2024-01-24 13:07:29 +0300

    created the login page and added all the contents in it

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,14 +3,106 @@ import styled from "styled-components";
 function Login() {
   return (
     <Container>
-      <Content>Content</Content>
+      <Content>
+        <CTA>
+          <CTALogoOne src="/images/cta-logo-one.svg" alt="logo" />
+          <SignUp>GET ALL THERE</SignUp>
+          <Description>
+            Get Premier Access to Raya and the Last Dragon for an additional fee
+            with a Disney+ subscription. As of 03/26/21, the price of Disney+
+            and The Disney Bundle will increase by $1.
+          </Description>
+          <CTALogoTwo
+            src="/images/cta-logo-two.png"
+            alt="logo two of the disney plus website"
+          />
+        </CTA>
+
+        <BgImage />
+      </Content>
     </Container>
   );
 }
 
-const Container = styled.div`
+const Container = styled.section`
   overflow: hidden;
   display: flex;
+  flex-direction: column;
+  text-align: center;
+  height: 100vh;
 `;
-const Content = styled.div``;
+const Content = styled.div`
+  margin-bottom: 10vw;
+  width: 100%;
+  position: relative;
+  min-height: 100vh;
+  box-sizing: border-box;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 80px 40px;
+  height: 100%;
+`;
+const BgImage = styled.div`
+  background-image: url("/images/login-background.jpg");
+  z-index: -1;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-size: cover;
+`;
+const CTA = styled.div`
+  margin-bottom: 2vw;
+  max-width: 650px;
+  flex-wrap: wrap;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin-top: 0;
+  align-items: center;
+  margin-right: auto;
+  margin-left: auto;
+  transition-timing-function: ease-out;
+  transition: opacity 0.2s;
+  width: 100%;
+`;
+const CTALogoOne = styled.img`
+  margin-bottom: 12px;
+  max-width: 600px;
+  min-height: 1px;
+  display: block;
+`;
+const CTALogoTwo = styled.img`
+  width: 100%;
+  max-width: 600px;
+  margin-bottom: 20px;
+  vertical-align: bottom;
+`;
+const SignUp = styled.a`
+  font-weight: bold;
+  color: #f9f9f9;
+  background-color: #0063e5;
+  padding: 16px 0;
+  width: 100%;
+  font-size: 18.5px;
+  letter-spacing: 1.5px;
+  margin-bottom: 12px;
+  border: 1px solid transparent;
+  border-radius: 5px;
+
+  &:hover {
+    background-color: #0483ee;
+  }
+`;
+
+const Description = styled.p`
+  color: hsla(0, 0%, 95.3%, 1);
+  font-size: 11px;
+  margin: 0 0 24px;
+  line-height: 1.5;
+  letter-spacing: 1.5px;
+`;
 export default Login;
